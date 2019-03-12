674997473b3ba1c00b658856e9bd803823d5cff6
luishens01 <luis.hens@atos.net>
2019-03-12 15:59:33 +0100

    change response allow

--- main.py
+++ main.py
@@ -386,8 +386,8 @@ CORS(app)
 
 @app.after_request
 def after_request(response):
-  response.headers.add('Access-Control-Allow-Origin', '*')
-  response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+  #response.headers.add('Access-Control-Allow-Origin', '*')
+  #response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
   response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS,PATCH')
   return response
 
